0aff1ee326c7525f9ade4334671d0e42c9dfa9ec
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-03-01 03:36:51 +0600

    spinner added

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,26 @@
 const searchButton = () => {
+  document.getElementById("founded-results").textContent = "";
+  document.getElementById("spinner").style.display = "block";
   const inputField = document.getElementById("input-field");
   const inputValue = inputField.value;
-  const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => getPhonesData(data.data));
-
   inputField.value = "";
+  if (inputValue == "" || inputValue == "number") {
+      alert('dfdkj')
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.data.length == 0) {
+          document.getElementById("spinner").style.display = "block";
+          document.getElementById("wrong-message").style.display = "block";
+        } else {
+          getPhonesData(data.data);
+          document.getElementById("wrong-message").style.display = "none";
+          document.getElementById("spinner").style.display = "none";
+        }
+      });
+  }
 };
 const getPhonesData = (allPhones) => {
   allPhones.forEach((phone) => {
@@ -39,6 +53,7 @@ const showDetailsInfo = (information) => {
   console.log(information);
   const displayInfo = document.getElementById("details-info");
   const div = document.createElement("div");
+  const releaseDate = document.getElementById("release-date");
   div.className = "row g-0";
   div.innerHTML = `
   <div class="col-md-4">
@@ -47,7 +62,7 @@ const showDetailsInfo = (information) => {
 <div class="col-md-8">
   <div class="card-body">
       <h5 class="card-title">${information.name}</h5>
-      <p class="text-muted">${information.releaseDate}</p>
+      <p id="release-date" class="text-muted">${information.releaseDate}</p>
       <p>Main Features: <b>Chipset:</b> ${information.mainFeatures.chipSet}, <b>Display-Size:</b> ${information.mainFeatures.displaySize}, <b>Memory:</b> ${information.mainFeatures.memory}</p>
       
   </div>
