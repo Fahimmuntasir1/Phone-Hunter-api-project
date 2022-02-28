9bfe4da421086423e5a1d386a996ec8f3be05ed7
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-03-01 01:06:35 +0600

    details section updeted

--- JS/app.js
+++ JS/app.js
@@ -36,12 +36,22 @@ const detailsBtn = (detailsCode) => {
 };
 
 const showDetailsInfo = (information) => {
-    console.log(information);
+  console.log(information);
   const displayInfo = document.getElementById("details-info");
   const div = document.createElement("div");
   div.className = "row g-0";
   div.innerHTML = `
-    
+  <div class="col-md-4">
+  <img src="${information.image}" class="img-fluid rounded-start" alt="...">
+</div>
+<div class="col-md-8">
+  <div class="card-body">
+      <h5 class="card-title">${information.name}</h5>
+      <p class="text-muted">${information.releaseDate}</p>
+      <p>Main Features: <b>Chipset:</b> ${information.mainFeatures.chipSet}, <b>Display-Size:</b> ${information.mainFeatures.displaySize}, <b>Memory:</b> ${information.mainFeatures.memory}</p>
+      
+  </div>
+</div>
   `;
   displayInfo.appendChild(div);
 };
